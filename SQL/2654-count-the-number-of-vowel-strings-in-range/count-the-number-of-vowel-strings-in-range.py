8952e8990e84b5545651e3c781c74e2228d445ed
“yejin <yhwang@islander.tamucc.edu>
2025-07-31 13:41:05 -0500

    misclassified

--- SQL/2654-count-the-number-of-vowel-strings-in-range/count-the-number-of-vowel-strings-in-range.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def vowelStrings(self, words: List[str], left: int, right: int) -> int:
-        vowel = ["a","e","i","o","u"]
-        count=0 
-        for i in range(len(words)): 
-            if words[i][0] in vowel and words[i][-1] in vowel:
-                if left<=i<=right: 
-                    count+=1
-        return count
