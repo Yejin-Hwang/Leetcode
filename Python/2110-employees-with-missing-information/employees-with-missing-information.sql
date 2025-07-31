9e3f23a965e88b5a4988e138652baad8a517d55a
Yejin Hwang <yhwang@islander.tamucc.edu>
2025-07-31 13:46:41 -0500

    Delete Python/2110-employees-with-missing-information directory

--- Python/2110-employees-with-missing-information/employees-with-missing-information.sql
+++
@@ -1,6 +0,0 @@
--- Write your PostgreSQL query statement below
-with cte as (select COALESCE(e.employee_id, s.employee_id) AS employee_id, e.name, s.salary
-from employees e full join salaries s on e.employee_id = s.employee_id) 
-
-select employee_id 
-from cte where name is null or salary is null order by employee_id asc; 
