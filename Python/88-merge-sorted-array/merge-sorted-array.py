b34ed2e164b47813dfebf0cbd286858fa175a507
Yejin Hwang <yhwang@islander.tamucc.edu>
2026-03-05 23:34:47 -0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (73.61%) - LeetSync

--- Python/88-merge-sorted-array/merge-sorted-array.py
+++ Python/88-merge-sorted-array/merge-sorted-array.py
@@ -3,11 +3,26 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        #Three pointers
+        nums1_copy = nums1[:m]
 
+        p1 = 0 #m
+        p2 = 0 #n
+
+        for p in range(n+m): 
+            if p2>=n or (p1<m and nums1_copy[p1]<nums2[p2]): 
+                nums1[p]=nums1_copy[p1]
+                p1+=1
+            else: 
+                nums1[p]=nums2[p2]
+                p2+=1
+
+
+        #---------------------------
         # Merge and sort 
 
-        for i in range (n): 
-            nums1[i+m]=nums2[i]
+        # for i in range (n): 
+        #     nums1[i+m]=nums2[i]
 
-        nums1.sort()
+        # nums1.sort()
         
